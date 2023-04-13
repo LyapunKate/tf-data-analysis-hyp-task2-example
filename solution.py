6c5ba3cdcdd6a4c1ec39ca0ef0822913f9fbd6e0
LyapunKate <91290812+LyapunKate@users.noreply.github.com>
2023-04-13 23:52:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 532569024 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.09
+    n1 = len(x)
+    n2 = len(y)
+    c_alpha = np.sqrt(-0.5*np.log(alpha/2))/np.sqrt(n1+n2)
+    d = np.max(np.abs(np.sort(x)-np.sort(y)))
+    return d > c_alpha
